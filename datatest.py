95d07ba3b938d534e9cfbae0c1f08104d0600883
Sri Harsha <srisatyaharsha29@gmail.com>
2026-02-19 15:30:56 +0530

    Implemented logistic regression both scratch and sklearn and results

--- datatest.py
+++ datatest.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 #initial understanding of the data
 df = pd.read_csv("churn.csv")
@@ -39,6 +40,98 @@ import matplotlib.pyplot as plt
 #plt.show()
 
 #df.groupby("Churn")["MonthlyCharges"].mean()
-x=pd.crosstab(df["Contract"], df["Churn"], normalize="index")
-print(x)
+#x=pd.crosstab(df["Contract"], df["Churn"], normalize="index")
+#print(x)
 
+df["Churn_binary"] = df["Churn"].map({"No": 0, "Yes": 1})
+print(df[["Churn", "Churn_binary"]].head())
+
+X = pd.get_dummies(
+    df[["tenure", "MonthlyCharges", "Contract"]],
+    drop_first=True
+)
+
+y = df["Churn_binary"]
+
+print(X.head())
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    stratify=y,
+    random_state=42
+)
+
+print(y_train.mean(), y_test.mean())
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def compute_loss(y, y_hat):
+    eps = 1e-15
+    return -np.mean(
+        y * np.log(y_hat + eps) +
+        (1 - y) * np.log(1 - y_hat + eps)
+    )
+
+def train_logistic(X, y, lr=0.01, epochs=1000):
+    n_samples, n_features = X.shape
+    w = np.zeros(n_features)
+    b = 0
+    losses = []
+
+    for _ in range(epochs):
+        z = X @ w + b
+        y_hat = sigmoid(z)
+
+        loss = compute_loss(y, y_hat)
+        losses.append(loss)
+
+        dw = (1 / n_samples) * (X.T @ (y_hat - y))
+        db = np.mean(y_hat - y)
+
+        w -= lr * dw
+        b -= lr * db
+
+    return w, b, losses
+w, b, losses = train_logistic(
+    X_train_scaled,
+    y_train.values,
+    lr=0.1,
+    epochs=1000
+)
+#import matplotlib.pyplot as plt
+
+#plt.plot(losses)
+#plt.xlabel("Epoch")
+#plt.ylabel("Loss")
+#plt.title("Training Loss")
+#plt.show()
+
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression(max_iter=1000)
+lr.fit(X_train_scaled, y_train)
+
+y_pred = lr.predict(X_test_scaled)
+y_prob = lr.predict_proba(X_test_scaled)[:, 
+
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)00
+from sklearn.metrics import classification_report
+
+print(classification_report(y_test, y_pred))
+import numpy as np
+
+print(np.percentile(y_prob, [10, 50, 90]))
